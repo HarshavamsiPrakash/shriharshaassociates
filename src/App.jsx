1bb50e648e2d6e0d8b627dc41fad0aacf557e92f
HarshavamsiPrakash <harshavamsiprakash566@gmail.com>
2026-02-08 00:06:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,3 @@
-// import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-// import { useEffect } from "react";
-
-// /* COMPONENTS */
-// import Header from "./components/Header";
-// import Hero from "./components/Hero";
-// import Services from "./components/Services";
-// import Projects from "./components/Projects";
-// import Testimonials from "./components/Testimonials";
-// import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
-
-// /* PAGES */
-// import AboutPage from "./pages/AboutPage";
-
-// /* SCROLL FIX COMPONENT */
-// function ScrollToSection() {
-//   const location = useLocation();
-
-//   useEffect(() => {
-//     if (location.hash) {
-//       const el = document.querySelector(location.hash);
-//       if (el) {
-//         setTimeout(() => {
-//           el.scrollIntoView({ behavior: "smooth", block: "start" });
-//         }, 100); // wait for DOM render
-//       }
-//     }
-//   }, [location]);
-
-//   return null;
-// }
-
-// export default function App() {
-//   return (
-//     <BrowserRouter>
-//       <Header />
-//       <ScrollToSection />
-
-//       <Routes>
-//         <Route
-//           path="/"
-//           element={
-//             <>
-//               <Hero />
-//               <Services />
-//               <Projects />
-//               <Testimonials />
-//               <Contact />
-//               <Footer />
-//             </>
-//           }
-//         />
-
-//         <Route path="/about" element={<AboutPage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
 
 
 // import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
@@ -65,6 +6,7 @@
 // /* COMPONENTS */
 // import Header from "./components/Header";
 // import Hero from "./components/Hero";
+// import Stats from "./components/Stats";
 // import Services from "./components/Services";
 // import Projects from "./components/Projects";
 // import Testimonials from "./components/Testimonials";
@@ -104,6 +46,7 @@
 //           element={
 //             <>
 //               <Hero />
+//               <Stats />
 //               <Services />
 //               <Projects />
 //               <Testimonials />
@@ -118,6 +61,7 @@
 //     </BrowserRouter>
 //   );
 // }
+
 
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
@@ -134,8 +78,20 @@ import Footer from "./components/Footer";
 
 /* PAGES */
 import AboutPage from "./pages/AboutPage";
+import ResidualsPage from "./pages/ResidualsPage";
 
-/* SCROLL FIX COMPONENT */
+/* SCROLL TO TOP ON ROUTE CHANGE */
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
+/* SCROLL TO SECTION (for hash navigation) */
 function ScrollToSection() {
   const location = useLocation();
 
@@ -157,6 +113,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Header />
+      <ScrollToTop />
       <ScrollToSection />
 
       <Routes>
@@ -176,6 +133,7 @@ export default function App() {
         />
 
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/residuals" element={<ResidualsPage />} />
       </Routes>
     </BrowserRouter>
   );
